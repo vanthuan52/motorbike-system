329cec175614cd106d05f8bab74be7c3ffd00152
ledangtrinh <ledangtrinh.05052002@gmail.com>
2025-05-13 16:56:26 +0700

    feat/technician-page

---
+++ frontend/src/app/(technician)/technician/maintenance/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import MaintenancePage from "@/components/technician/Maintenance/MaintenancePage";
+export default function page() {
+  return <MaintenancePage />;
+}
---
+++ frontend/src/app/(technician)/technician/page.tsx
@@ -0,0 +1,3 @@
+export default function TechnicianHomePage() {
+  return <main className="p-6"></main>;
+}
