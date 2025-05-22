6d6b8d03c8d66ed5390a6a527179429f5ddf0159
Quaniscoding <vq.2509.2003@gmail.com>
2025-05-22 15:48:40 +0700

    feat: implement ToDo list page and enhance maintenance details handling

---
+++ frontend/src/app/(technician)/technician/maintenance/to-do-list/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+import React from 'react'
+import ToDoListPage from './components/ToDoListPage'
+
+export const metadata: Metadata = {
+    title: 'Danh sách hạng mục công việc',
+    description: 'Chi tiết công việc bảo trì',
+}
+export default function page() {
+  return (
+   <ToDoListPage/>
+  )
+}
