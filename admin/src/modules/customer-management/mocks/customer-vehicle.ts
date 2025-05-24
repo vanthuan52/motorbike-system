f537dc379fbe7ded4f8ff224ecc59f971b444c57
ledangtrinh <ledangtrinh.05052002@gmail.com>
2025-05-25 00:03:17 +0700

    trinh/move-frontend-admin

---
+++ admin/src/modules/customer-management/mocks/customer-vehicle.ts
@@ -0,0 +1,27 @@
+
+import { Vehicle } from "../../../types/vehicle";
+
+export const vehicleData: Vehicle[] = [
+  {
+    id: "v001",
+    customer_id: "cus-01",
+    license_plate: "51A-123.45",
+    vehicle_model: "Toyota Vios",
+    color: "Trắng",
+    engine_number: "EN123456",
+    chassis_number: "CH123456",
+    vehicle_type_id: "vt-1",
+    image_file_name: "/images/banner/hero-banner-bike.jpg",
+  },
+  {
+    id: "v002",
+    customer_id: "cus-01",
+    license_plate: "51B-987.65",
+    vehicle_model: "Honda Civic",
+    color: "Đỏ",
+    engine_number: "EN654321",
+    chassis_number: "CH654321",
+    vehicle_type_id: "vt-2",
+    image_file_name: "/images/banner/hero-banner-bike.jpg",
+  },
+];
---
+++ admin/src/modules/vehicle-parts/types/types.ts
@@ -0,0 +1,11 @@
+export interface VehiclePart {
+  id: string;
+  vehicle_type_id: string | null;
+  name: string;
+  code: string;
+  average_life: number;
+  unit_price: number;
+  quantity: number;
+  status: boolean;
+  images?: string[];
+}
