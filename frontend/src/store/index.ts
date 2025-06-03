4946b7c1e05b7acb028868b1978332386bdd8be6
vanthuan52 <thuan.jolly@gmail.com>
2025-06-03 22:20:05 +0700

    feat: add basic redux store

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./root-saga";
+import { authReducer } from "./features/auth";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
