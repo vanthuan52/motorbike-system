bdd4bf7a8f5c97d9fa5d1ab3c59a7414d5941c94
quaniscoding <vq.2509.2003@gmail.com>
2025-08-20 22:27:43 +0700

    feat: add VehicleModelModal component for vehicle model selection with filtering options

--- frontend/src/features/appointment/hooks/useVehicleModelByBrand.ts
+++ frontend/src/features/appointment/hooks/useVehicleModelByBrand.ts
@@ -1,13 +1,23 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { vehicleModelActions } from "@/features/vehicle-model/store/vehicle-model-slice";
+import {
+  ENUM_VEHICLE_MODEL_FUEL_TYPE,
+  ENUM_VEHICLE_MODEL_TYPE,
+} from "@/features/vehicle-model/types";
 
-export const useVehicleModelByBrand = (vehicleBrandId?: string) => {
+export const useVehicleModelByBrand = (
+  vehicleBrandId?: string,
+  filters?: {
+    search?: string;
+    engineDisplacement?: number;
+    modelYear?: number;
+    fuelType?: ENUM_VEHICLE_MODEL_FUEL_TYPE;
+    type?: ENUM_VEHICLE_MODEL_TYPE;
+  }
+) => {
   const dispatch = useDispatch();
-  const [initialized, setInitialized] = useState(false);
 
   const { list: vehicleModels, loadingList: loadingVehicleModels } =
     useSelector((state: RootState) => state.vehicleModel);
@@ -16,21 +26,28 @@ export const useVehicleModelByBrand = (vehicleBrandId?: string) => {
     if (vehicleBrandId) {
       dispatch(
         vehicleModelActions.getVehicleModels({
+          search: filters?.search,
           vehicleBrand: vehicleBrandId,
           page: 1,
           perPage: 100,
+          engineDisplacement: filters?.engineDisplacement,
+          modelYear: filters?.modelYear,
+          fuelType: filters?.fuelType,
+          type: filters?.type,
         })
       );
-      setInitialized(true);
     } else {
       dispatch(vehicleModelActions.resetState());
     }
-  }, [vehicleBrandId, dispatch]);
+  }, [vehicleBrandId, dispatch, filters]);
 
   const vehicleModelOptions =
     vehicleModels.map((m) => ({
       label: m.name,
       value: m._id,
+      year: m.modelYear,
+      displacement: m.engineDisplacement,
+      image: m.photo,
     })) || [];
 
   return {
