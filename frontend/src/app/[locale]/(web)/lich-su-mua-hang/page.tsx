08b0433d2f0231637d1138e8c657fa34161e351d
vanthuan52 <thuan.jolly@gmail.com>
2026-04-13 17:57:53 +0700

    fix: swiper

--- frontend/src/app/[locale]/(web)/lich-su-mua-hang/page.tsx
+++ frontend/src/app/[locale]/(web)/lich-su-mua-hang/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import PurchaseHistoryPage from "../../../../features/order/components/purchase-history-page";
+import PurchaseHistoryPage from "@/features/order/components/purchase-history-page";
 
 export const metadata: Metadata = {
   title: "Lịch sử mua hàng",
